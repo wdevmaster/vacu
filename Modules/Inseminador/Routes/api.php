00823992a9c48a63155b6b563ef73652405a869b
castillochacon <castillochacon.cyl@gmail.com>
2020-05-04 09:01:16 -0400

    Added documentation for module Inseminador

--- Modules/Inseminador/Routes/api.php
+++ Modules/Inseminador/Routes/api.php
@@ -16,3 +16,5 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/inseminador', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('inseminadors', 'InseminadorAPIController');
--- config/infyom/laravel_generator.php
+++ config/infyom/laravel_generator.php
@@ -1,6 +1,6 @@
 <?php
 
-$module = 'Ingreso';
+$module = 'Inseminador';
 
 return [
 
