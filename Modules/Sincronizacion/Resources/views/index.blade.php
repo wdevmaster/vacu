d0edb926a4f5c93826e66eeda81088c3ded0b3e5
castillochacon <castillochacon.cyl@gmail.com>
2020-05-05 09:00:14 -0400

    Added Sincronizacion module

---
+++ Modules/Sincronizacion/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('sincronizacion::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('sincronizacion.name') !!}
+    </p>
+@endsection
