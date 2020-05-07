970dda33b552a091bfee14edb525e9a2dc6e9b6c
castillochacon <castillochacon.cyl@gmail.com>
2020-05-07 13:08:45 -0400

    Added entity and migration  for translate

---
+++ Modules/Sincronizacion/Database/Migrations/2020_05_07_170529_create_traductors_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateTraductorsTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('traducciones', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('user_code');
+            $table->string('generate_code');
+            $table->string('tabla');
+
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('traducciones');
+    }
+}
---
+++ Modules/Sincronizacion/Entities/Traductor.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Modules\Sincronizacion\Entities;
+
+use App\Models\Eloquent as Model;
+
+/**
+ * @SWG\Definition(
+ *      definition="Traductor",
+ *      required={"user_id", "user_code", "generate_code"},
+ *      @SWG\Property(
+ *          property="id",
+ *          description="id",
+ *          type="integer",
+ *          format="int32"
+ *      ),
+ *      @SWG\Property(
+ *          property="user_id",
+ *          description="user_id",
+ *          type="integer",
+ *          format="int32"
+ *      ),
+ *      @SWG\Property(
+ *          property="user_code",
+ *          description="user_code",
+ *          type="string"
+ *      ),
+ *      @SWG\Property(
+ *          property="generate_code",
+ *          description="generate_code",
+ *          type="string"
+ *      ),
+ *      @SWG\Property(
+ *          property="tabla",
+ *          description="tabla",
+ *          type="string"
+ *      ),
+ *      @SWG\Property(
+ *          property="created_at",
+ *          description="created_at",
+ *          type="string",
+ *          format="date-time"
+ *      ),
+ *      @SWG\Property(
+ *          property="updated_at",
+ *          description="updated_at",
+ *          type="string",
+ *          format="date-time"
+ *      )
+ * )
+ */
+class Traductor extends Model
+{
+
+    public $table = 'traducciones';
+    
+
+
+
+    public $fillable = [
+        'user_id',
+        'user_code',
+        'generate_code',
+        'tabla'
+    ];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'user_id' => 'integer',
+        'user_code' => 'string',
+        'generate_code' => 'string',
+        'tabla' => 'string'
+    ];
+
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    public static $rules = [
+        'user_id' => 'required',
+        'user_code' => 'required',
+        'generate_code' => 'required',
+        'tabla' => 'requerid'
+    ];
+
+    
+}
