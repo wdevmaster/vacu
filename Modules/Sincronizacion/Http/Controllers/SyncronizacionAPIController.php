a5ba9ad7335a201e0e1aa9955477a5cfcd46d0be
castillochacon <castillochacon.cyl@gmail.com>
2020-05-08 12:37:38 -0400

    Actualizada la ruta para inicio de syncronizacion

--- Modules/Sincronizacion/Http/Controllers/SyncronizacionAPIController.php
+++ Modules/Sincronizacion/Http/Controllers/SyncronizacionAPIController.php
@@ -3,11 +3,9 @@
 namespace Modules\Sincronizacion\Http\Controllers;
 
 use App\Http\Controllers\AppBaseController;
-use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Response;
-use Modules\Sincronizacion\Entities\Syncronizacion;
 use Modules\Sincronizacion\Http\Requests\CreateSyncronizacionAPIRequest;
-use Modules\Sincronizacion\Http\Requests\UpdateSyncronizacionAPIRequest;
 use Modules\Sincronizacion\Repositories\SyncronizacionRepository;
 use Modules\Sincronizacion\Services\SyncDataServiceInterface;
 
@@ -32,54 +30,11 @@ class SyncronizacionAPIController extends AppBaseController
     }
 
     /**
-     * @param Request $request
-     * @return Response
-     *
-     * @SWG\Get(
-     *      path="/syncronizacions",
-     *      summary="Get a listing of the Syncronizacions.",
-     *      tags={"Syncronizacion"},
-     *      description="Get all Syncronizacions",
-     *      produces={"application/json"},
-     *      @SWG\Response(
-     *          response=200,
-     *          description="successful operation",
-     *          @SWG\Schema(
-     *              type="object",
-     *              @SWG\Property(
-     *                  property="success",
-     *                  type="boolean"
-     *              ),
-     *              @SWG\Property(
-     *                  property="data",
-     *                  type="array",
-     *                  @SWG\Items(ref="#/definitions/Syncronizacion")
-     *              ),
-     *              @SWG\Property(
-     *                  property="message",
-     *                  type="string"
-     *              )
-     *          )
-     *      )
-     * )
-     */
-    public function index(Request $request)
-    {
-        $syncronizacions = $this->syncronizacionRepository->all(
-            $request->except(['skip', 'limit']),
-            $request->get('skip'),
-            $request->get('limit')
-        );
-
-        return $this->sendResponse($syncronizacions->toArray(), 'Syncronizacions retrieved successfully');
-    }
-
-    /**
      * @param CreateSyncronizacionAPIRequest $request
      * @return Response
      *
      * @SWG\Post(
-     *      path="/syncronizacions",
+     *      path="/api/v1/sincronizacion/sincronizaciones",
      *      summary="Store a newly created Syncronizacion in storage",
      *      tags={"Syncronizacion"},
      *      description="Store Syncronizacion",
@@ -114,178 +69,36 @@ class SyncronizacionAPIController extends AppBaseController
      */
     public function store(CreateSyncronizacionAPIRequest $request)
     {
-        $input = $request->all();
+        try {
+            $input = $request->all();
 
-        $syncronizacion = $this->syncronizacionRepository->create($input);
+            $sincronizacion = $this->syncronizacionRepository->create($input);
 
-        return $this->sendResponse($syncronizacion->toArray(), 'Syncronizacion saved successfully');
-    }
-
-    /**
-     * @param int $id
-     * @return Response
-     *
-     * @SWG\Get(
-     *      path="/syncronizacions/{id}",
-     *      summary="Display the specified Syncronizacion",
-     *      tags={"Syncronizacion"},
-     *      description="Get Syncronizacion",
-     *      produces={"application/json"},
-     *      @SWG\Parameter(
-     *          name="id",
-     *          description="id of Syncronizacion",
-     *          type="integer",
-     *          required=true,
-     *          in="path"
-     *      ),
-     *      @SWG\Response(
-     *          response=200,
-     *          description="successful operation",
-     *          @SWG\Schema(
-     *              type="object",
-     *              @SWG\Property(
-     *                  property="success",
-     *                  type="boolean"
-     *              ),
-     *              @SWG\Property(
-     *                  property="data",
-     *                  ref="#/definitions/Syncronizacion"
-     *              ),
-     *              @SWG\Property(
-     *                  property="message",
-     *                  type="string"
-     *              )
-     *          )
-     *      )
-     * )
-     */
-    public function show($id)
-    {
-        /** @var Syncronizacion $syncronizacion */
-        $syncronizacion = $this->syncronizacionRepository->find($id);
-
-        if (empty($syncronizacion)) {
-            return $this->sendError('Syncronizacion not found');
+            return response()->json([
+                'message' => __('comun::msgs.la_model_saved_successfully', [
+                    'model' => trans_choice('configuracion::msgs.label_sincronizacion', 1)
+                ]),
+                'success' => true,
+                'data' => $sincronizacion
+            ], 200);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'message' => __('comun::msgs.la_model_not_found', [
+                    'model' => trans_choice('usuario::msgs.label_sincronizacion', 1)
+                ]),
+                'success' => false
+            ], 404);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => __('comun::msgs.msg_error_contact_the_administrator'),
+                'success' => false
+            ], 500);
         }
-
-        return $this->sendResponse($syncronizacion->toArray(), 'Syncronizacion retrieved successfully');
     }
 
-    /**
-     * @param int $id
-     * @param UpdateSyncronizacionAPIRequest $request
-     * @return Response
-     *
-     * @SWG\Put(
-     *      path="/syncronizacions/{id}",
-     *      summary="Update the specified Syncronizacion in storage",
-     *      tags={"Syncronizacion"},
-     *      description="Update Syncronizacion",
-     *      produces={"application/json"},
-     *      @SWG\Parameter(
-     *          name="id",
-     *          description="id of Syncronizacion",
-     *          type="integer",
-     *          required=true,
-     *          in="path"
-     *      ),
-     *      @SWG\Parameter(
-     *          name="body",
-     *          in="body",
-     *          description="Syncronizacion that should be updated",
-     *          required=false,
-     *          @SWG\Schema(ref="#/definitions/Syncronizacion")
-     *      ),
-     *      @SWG\Response(
-     *          response=200,
-     *          description="successful operation",
-     *          @SWG\Schema(
-     *              type="object",
-     *              @SWG\Property(
-     *                  property="success",
-     *                  type="boolean"
-     *              ),
-     *              @SWG\Property(
-     *                  property="data",
-     *                  ref="#/definitions/Syncronizacion"
-     *              ),
-     *              @SWG\Property(
-     *                  property="message",
-     *                  type="string"
-     *              )
-     *          )
-     *      )
-     * )
-     */
-    public function update($id, UpdateSyncronizacionAPIRequest $request)
+
+    public function startSync()
     {
-        $input = $request->all();
-
-        /** @var Syncronizacion $syncronizacion */
-        $syncronizacion = $this->syncronizacionRepository->find($id);
-
-        if (empty($syncronizacion)) {
-            return $this->sendError('Syncronizacion not found');
-        }
-
-        $syncronizacion = $this->syncronizacionRepository->update($input, $id);
-
-        return $this->sendResponse($syncronizacion->toArray(), 'Syncronizacion updated successfully');
-    }
-
-    /**
-     * @param int $id
-     * @return Response
-     *
-     * @SWG\Delete(
-     *      path="/syncronizacions/{id}",
-     *      summary="Remove the specified Syncronizacion from storage",
-     *      tags={"Syncronizacion"},
-     *      description="Delete Syncronizacion",
-     *      produces={"application/json"},
-     *      @SWG\Parameter(
-     *          name="id",
-     *          description="id of Syncronizacion",
-     *          type="integer",
-     *          required=true,
-     *          in="path"
-     *      ),
-     *      @SWG\Response(
-     *          response=200,
-     *          description="successful operation",
-     *          @SWG\Schema(
-     *              type="object",
-     *              @SWG\Property(
-     *                  property="success",
-     *                  type="boolean"
-     *              ),
-     *              @SWG\Property(
-     *                  property="data",
-     *                  type="string"
-     *              ),
-     *              @SWG\Property(
-     *                  property="message",
-     *                  type="string"
-     *              )
-     *          )
-     *      )
-     * )
-     */
-    public function destroy($id)
-    {
-        /** @var Syncronizacion $syncronizacion */
-        $syncronizacion = $this->syncronizacionRepository->find($id);
-
-        if (empty($syncronizacion)) {
-            return $this->sendError('Syncronizacion not found');
-        }
-
-        $syncronizacion->delete();
-
-        return $this->sendSuccess('Syncronizacion deleted successfully');
-    }
-
-    public function startSync(){
         $this->syncDataService->executeService();
     }
 }
--- Modules/Sincronizacion/Routes/api.php
+++ Modules/Sincronizacion/Routes/api.php
@@ -17,5 +17,18 @@ Route::middleware('auth:api')->get('/sincronizacion', function (Request $request
     return $request->user();
 });
 
+
+Route::prefix('v1/sincronizacion')->group(function(){
+
+    Route::prefix('sincronizaciones')->group(function () {
+//        Route::get('/', 'SyncronizacionAPIController@index')->name('configuracion.index');
+        Route::post('/', 'SyncronizacionAPIController@store')->name('configuracion.store');
+//        Route::put('/{id}', 'SyncronizacionAPIController@update')->name('configuracion.update');
+//        Route::delete('/{id}', 'SyncronizacionAPIController@destroy')->name('configuracion.delete');
+    });
+
+});
+
+
 Route::resource('syncronizacions', 'SyncronizacionAPIController');
 Route::post('/sync', 'SyncronizacionAPIController@startSync')->name('sync.data');
