b27f48f261f03e2c0fbe88b62677bc804bc1be59
castillochacon <castillochacon.cyl@gmail.com>
2020-06-09 21:19:39 -0400

    Updated routes in module cliente

--- Modules/Cliente/Routes/api.php
+++ Modules/Cliente/Routes/api.php
@@ -15,8 +15,8 @@ Route::prefix('v1/cliente')->middleware('auth:api')->group(function () {
     Route::prefix('/clientes')->group(function () {
         Route::get('/', 'ClienteAPIController@index')->name('cliente.clientes.index');
         Route::post('/', 'ClienteAPIController@store')->name('cliente.clientes.store');
-        Route::get('/{id}', 'ClienteAPIController@update')->name('cliente.clientes.update');
-        Route::get('/{id}', 'ClienteAPIController@destroy')->name('cliente.clientes.destroy');
+        Route::put('/{id}', 'ClienteAPIController@update')->name('cliente.clientes.update');
+        Route::delete('/{id}', 'ClienteAPIController@destroy')->name('cliente.clientes.destroy');
     });
 });
 
