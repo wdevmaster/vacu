c9eed729f0f48bbf232c36758526360896f79d04
castillochacon <castillochacon.cyl@gmail.com>
2020-07-13 17:55:03 -0400

    Updated Cliente to seeder

--- Modules/Cliente/Database/factories/ClienteFactory.php
+++ Modules/Cliente/Database/factories/ClienteFactory.php
@@ -11,7 +11,7 @@ $factory->define(Cliente::class, function (Faker $faker) {
         'code' => $faker->word,
         'nombre' => $faker->word,
         'descripcion' => $faker->word,
-        'telfono' => $faker->word,
+        'telefono' => $faker->word,
         'active' => $faker->word,
         'negocio_id' => $faker->randomDigitNotNull,
         'created_at' => $faker->date('Y-m-d H:i:s'),
