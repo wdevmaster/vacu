7ae1d1bc1bed8dd7c781d2941750fe65fae4be2f
castillochacon <castillochacon.cyl@gmail.com>
2020-08-09 22:54:21 -0400

    Updated  dates format

--- app/Models/Eloquent.php
+++ app/Models/Eloquent.php
@@ -9,9 +9,23 @@
 namespace App\Models;
 
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Eloquent extends Model
 {
+    protected $dates = ['fecha', 'fecha_nacimiento'];
+
+    public function setFechaAttribute($value)
+    {
+
+        $this->attributes['fecha'] = Carbon::parse($value)->format('Y-m-d H:i:s');
+    }
+
+    public function setFechaNacimientoAttribute($value)
+    {
+        $this->attributes['fecha_nacimiento'] = Carbon::parse($value)->format('Y-m-d H:i:s');
+    }
+
 
 }
