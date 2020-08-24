9f1924dc657a4531a1752d14a966eeb7da9a851d
castillochacon <castillochacon.cyl@gmail.com>
2020-08-24 15:12:58 -0400

    Updated logic to delete in sync

--- Modules/Common/Resolvers/BaseResolver.php
+++ Modules/Common/Resolvers/BaseResolver.php
@@ -11,13 +11,11 @@ namespace Modules\Common\Resolvers;
 
 use App\Repositories\BaseRepository;
 use Carbon\Carbon;
-use Illuminate\Database\Eloquent\Model;
 use Modules\Bitacora\Entities\Bitacora;
 use Modules\Bitacora\Repositories\BitacoraRepository;
 use Modules\Sincronizacion\Entities\Syncronizacion;
 use Modules\Sincronizacion\Repositories\TraductorRepository;
 use Modules\Usuario\Repositories\UserRepository;
-use phpDocumentor\Reflection\Types\Integer;
 
 class BaseResolver
 {
@@ -32,7 +30,7 @@ class BaseResolver
 
     protected $bitacoraRepository;
 
-    public function __construct(BitacoraRepository $bitacoraRepository,TraductorRepository $traductorRepository, GenerateCodeResolverInterface $generateCodeResolver,UserRepository $userRepo)
+    public function __construct(BitacoraRepository $bitacoraRepository, TraductorRepository $traductorRepository, GenerateCodeResolverInterface $generateCodeResolver, UserRepository $userRepo)
     {
         $this->traductorRepository = $traductorRepository;
         $this->generateCodeResolver = $generateCodeResolver;
@@ -40,17 +38,17 @@ class BaseResolver
         $this->bitacoraRepository = $bitacoraRepository;
     }
 
-    public function handle(Syncronizacion $sincronizacion, BaseRepository $repository,$negocio_id)
+    public function handle(Syncronizacion $sincronizacion, BaseRepository $repository, $negocio_id)
     {
 
 
-        $user_id=$sincronizacion->user_id;
+        $user_id = $sincronizacion->user_id;
         $accion = $sincronizacion->accion;
         $data = json_decode(json_decode($sincronizacion->data, true));
-        $data = (array) $data;
+        $data = (array)$data;
 
 
-        if(isset($data['code'])) {
+        if (isset($data['code'])) {
             $code = $data['code'];
             $code_old = $data['code'];
             $data['negocio_id'] = $negocio_id;
@@ -64,18 +62,18 @@ class BaseResolver
             case 'INSERT':
                 $data['user_id'] = $sincronizacion->user_id;
 
-                if (isset($data['code'])){
+                if (isset($data['code'])) {
                     $validateCode = $repository->validateCode($code);
-                    $fecha_actual=Carbon::now()->toDateTimeString();
-                    if ($validateCode){
-                        $code = $this->generateCodeResolver->handle($code, $sincronizacion->tabla,$negocio_id,$user_id);
-                        if ($sincronizacion->tabla == 'animales'){
-                            $bitacora= new Bitacora();
-                            $bitacora->fecha_generacion= $fecha_actual;
-                            $bitacora->code_usuario=$code_old;
-                            $bitacora->code_generado= $code;
-                            $bitacora->entidad='animales';
-                            $bitacora->usuario_id=$user_id;
+                    $fecha_actual = Carbon::now()->toDateTimeString();
+                    if ($validateCode) {
+                        $code = $this->generateCodeResolver->handle($code, $sincronizacion->tabla, $negocio_id, $user_id);
+                        if ($sincronizacion->tabla == 'animales') {
+                            $bitacora = new Bitacora();
+                            $bitacora->fecha_generacion = $fecha_actual;
+                            $bitacora->code_usuario = $code_old;
+                            $bitacora->code_generado = $code;
+                            $bitacora->entidad = 'animales';
+                            $bitacora->usuario_id = $user_id;
 
                             $this->bitacoraRepository->create($bitacora->toArray());
 
@@ -91,11 +89,11 @@ class BaseResolver
 
             case 'UPDATE':
 
-                if(isset($data['code'])){
+                if (isset($data['code'])) {
                     $model = $repository->all()
                         ->where('code', '=', $code)
                         ->first();
-                }else{
+                } else {
                     $model = $repository->all()
                         ->where('id', '=', $data['id'])
                         ->first();
@@ -108,39 +106,24 @@ class BaseResolver
 
             case 'DELETE':
 
-                if(isset($data['code']) && !is_null($data['code'])){
+                if (isset($data['code']) && !is_null($data['code'])) {
                     $model = $repository->all()
                         ->where('code', '=', $code)
                         ->first();
                     if ($model) {
-                        if (isset($data['active'])){
-                            $repository->delete_active_off($model->id);
-                        }else{
-                            try {
-                                $repository->delete($model->id);
-                            } catch (\Exception $e) {
-                            }
-                        }
-
+                        $repository->delete_active_off($model->id);
                     }
 
-                }else{
+                } else {
                     $model = $repository->all()
                         ->where('id', '=', $data['id'])
                         ->first();
 
                     if ($model) {
-                        if (isset($data['active'])){
-                            $repository->delete_active_off($model->id);
-                        }else{
-                            try {
-                                $repository->delete($model->id);
-                            } catch (\Exception $e) {
-                            }
-                        }
+                        $repository->delete_active_off($model->id);
                     }
-                }
 
+                }
 
 
                 break;
